404bdc202f26f3a923d828fc679e289bf6c48ad2
Atif Aziz <code@raboof.com>
2014-04-16 09:06:13 +0200

    Build scripts

---
+++ build.cmd
@@ -0,0 +1,11 @@
+@echo off
+setlocal
+cd "%~dp0"
+set MSBUILDEXE=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
+if not exist "%MSBUILDEXE%" (
+    echo The .NET Framework 4.0 does not appear to be installed on this 
+    echo machine, which is required to build the solution.
+    exit /b 1
+)
+set EnableNuGetPackageRestore=true
+for %%s in (*.sln) do for %%c in (Debug Release) do "%MSBUILDEXE%" %%s /p:Configuration=%%c /v:m %*
