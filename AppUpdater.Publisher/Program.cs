6d6b3a6a3f45cb2cef96792b762527a8234e6174
Atif Aziz <code@raboof.com>
2014-04-16 23:15:16 +0200

    Refactored arg parsing, yielding a static Publisher Program class

--- AppUpdater.Publisher/Program.cs
+++ AppUpdater.Publisher/Program.cs
@@ -1,27 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.IO;
 
 namespace AppUpdater.Publisher
 {
-    class Program
+    static class Program
     {
-        private string sourceDirectory;
-        private string targetDirectory;
-        private string sourceDirectoryPath;
-        private string targetDirectoryPath;
-        private Version version;
-        private int? numberOfVersionsAsDelta = null;
-
         static int Main(string[] args)
         {
             try
             {
-                new Program().Run(args);
-                return 0;
+                return Run(args);
             }
             catch (Exception e)
             {
@@ -31,115 +21,92 @@ namespace AppUpdater.Publisher
             }
         }
 
-        public void Run(string[] args)
+        static int Run(IEnumerable<string> args)
         {
-            if (args.Length == 0)
+            string sourceDirectory;
+            string targetDirectory;
+            Version version;
+            var numberOfVersionsAsDelta = 0;
+            
+            using (var arg = args.GetEnumerator())
             {
-                Console.WriteLine("Usage:");
-                Console.WriteLine("  {0} -source:source_dir -target:target_dir -version:1.0.0 -deltas:2",
-                    Path.GetFileName(Environment.GetCommandLineArgs()[0]));
+                if (!arg.MoveNext())
+                {
+                    ShowUsage();
+                    return 1;
+                }
+
+                if (string.IsNullOrEmpty(sourceDirectory = arg.Current))
+                    throw new Exception("Missing source directory path specification.");
+                if (!Directory.Exists(sourceDirectory))
+                    throw new DirectoryNotFoundException("Source directory not found: " + sourceDirectory);
+
+                if (!arg.MoveNext() || string.IsNullOrEmpty(targetDirectory = arg.Current))
+                    throw new Exception("Missing target directory path specification.");
+                if (!Directory.Exists(targetDirectory))
+                    throw new DirectoryNotFoundException("Target directory not found: " + targetDirectory);
+
+                if (!arg.MoveNext())
+                    throw new Exception("Missing version specification.");
+                if (!Version.TryParse(arg.Current, out version))
+                    throw new Exception(string.Format("'{0}' is not a valid version specification.", arg.Current));
+
+                using (var option = ParseArgs(arg, (nm, cn, val) => new { Name = nm, CanonicalName = cn, Value = val }))
+                while (option.MoveNext())
+                {
+                    switch (option.Current.CanonicalName)
+                    {
+                        case "deltas":
+                            if (!int.TryParse(option.Current.Value, out numberOfVersionsAsDelta))
+                                throw new Exception("The 'delta' argument is not a valid number.");
+                            break;
+                        default:
+                            throw new Exception("Unknown argument: " + option.Current.Name);
+                    }
+                }
             }
-            else
-            {
-                ProcessArgs(args);
-                ValidateArgs();
-                PublishVersion();
-            }
+
+            PublishVersion(sourceDirectory, targetDirectory, version, numberOfVersionsAsDelta);
+            return 0;
         }
 
-        private void PublishVersion()
+        static void PublishVersion(string sourceDirectory, string targetDirectory, Version version, int numberOfVersionsAsDelta)
         {
             Console.WriteLine();
             Console.WriteLine("Publishing version \"{0}\"...", version);
-            Console.WriteLine("Source directory: {0}", sourceDirectoryPath);
-            Console.WriteLine("Target directory: {0}", targetDirectoryPath);
-            if (numberOfVersionsAsDelta.HasValue)
-            {
+            Console.WriteLine("Source directory: {0}", sourceDirectory);
+            Console.WriteLine("Target directory: {0}", targetDirectory);
+            if (numberOfVersionsAsDelta > 0)
                 Console.WriteLine("Generating delta information for the latest {0} versions.", numberOfVersionsAsDelta);
-            }
             Console.WriteLine();
 
-            AppPublisher.Publish(sourceDirectory, targetDirectoryPath, version, numberOfVersionsAsDelta ?? 0);
+            AppPublisher.Publish(sourceDirectory, targetDirectory, version, numberOfVersionsAsDelta);
 
             Console.WriteLine("Publish succeeded.");
         }
 
-        private void ProcessArgs(string[] args)
+        static IEnumerator<T> ParseArgs<T>(IEnumerator<string> e,
+            Func<string, string, string, T> selector)
         {
-            foreach (var arg in args)
+            while (e.MoveNext())
             {
+                var arg = e.Current;
                 if (!arg.StartsWith("-"))
-                {
                     throw new Exception("Invalid argument: " + arg);
-                }
 
-                var argValues = arg.Split(new []{':'}, 2);
-                var commandName = argValues[0].Remove(0, 1);
-                var commandValue = argValues.Length == 1 ? null : argValues[1];
-
-                switch (commandName.ToLower())
-                {
-                    case "source":
-                        sourceDirectory = commandValue;
-                        break;
-                    case "target":
-                        targetDirectory = commandValue;
-                        break;
-                    case "version":
-                        version = commandValue != null ? new Version(commandValue) : null;
-                        break;
-                    case "deltas":
-                        int deltas;
-                        if (!int.TryParse(commandValue, out deltas))
-                        {
-                            throw new Exception("The 'delta' argument is not a valid number.");
-                        }
-                        numberOfVersionsAsDelta = deltas;
-                        break;
-                    default:
-                        throw new Exception("Unknown argument: " + arg);
-                }
+                var tokens = arg.Split(new[] { ':' }, 2);
+                var name = tokens[0].Remove(0, 1);
+                yield return selector(name, 
+                                      name.ToLowerInvariant(),
+                                      tokens.Length == 1 ? null : tokens[1]);
             }
         }
 
-        private void ValidateArgs()
+        static void ShowUsage()
         {
-            var sb = new StringBuilder();
-            if (String.IsNullOrWhiteSpace(sourceDirectory))
-            {
-                sb.AppendLine("The 'source' argument is required.");
-            }
-            else
-            {
-                sourceDirectoryPath = Path.GetFullPath(sourceDirectory);
-                if (!Directory.Exists(sourceDirectoryPath))
-                {
-                    throw new Exception(String.Format("The directory '{0}' could not be found.", sourceDirectoryPath));
-                }
-            }
-
-            if (String.IsNullOrWhiteSpace(targetDirectory))
-            {
-                sb.AppendLine("The 'target' argument is required.");
-            }
-            else
-            {
-                targetDirectoryPath = Path.GetFullPath(targetDirectory);
-                if (!Directory.Exists(targetDirectoryPath))
-                {
-                    throw new Exception(String.Format("The directory '{0}' could not be found.", targetDirectoryPath));
-                }
-            }
-
-            if (version == null)
-            {
-                sb.AppendLine("The 'version' argument is required.");
-            }
-
-            if (sb.Length > 0)
-            {
-                throw new Exception(sb.ToString());
-            }
+            Console.WriteLine("Usage:");
+            Console.WriteLine("  {0} SOURCE_DIR TARGET_DIR VERSION [-deltas:NUM]",
+                Path.GetFileName(Environment.GetCommandLineArgs()[0]));
         }
     }
 }
