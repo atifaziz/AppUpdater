298230e109b51cabd1fdf936e05782d4d40e9ab2
Atif Aziz <code@raboof.com>
2014-04-18 00:39:47 +0200

    Replacing hex string code in Checksum.Calculate with BitConverter that's same

--- AppUpdater/Utils/Checksum.cs
+++ AppUpdater/Utils/Checksum.cs
@@ -1,30 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Security.Cryptography;
-using System.IO;
-
-namespace AppUpdater.Utils
+﻿namespace AppUpdater.Utils
 {
+    using System;
+    using System.Security.Cryptography;
+    using System.IO;
+
     public static class Checksum
     {
         public static string Calculate(Stream stream)
         {
-            if (stream == null)
-            {
-                throw new ArgumentNullException("stream");
-            }
-
+            if (stream == null) throw new ArgumentNullException("stream");
             var sha = new SHA256Managed();
             var checksum = sha.ComputeHash(stream);
-            var sb = new StringBuilder();
-            foreach (var b in checksum)
-            {
-                sb.Append(b.ToString("X2"));
-            }
-
-            return sb.ToString();
+            return BitConverter.ToString(checksum).Replace("-", string.Empty);
         }
     }
 }
